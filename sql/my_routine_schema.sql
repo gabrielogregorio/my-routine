df8eb8f8f431086d4aff66d082000428de491b1d
Fabiano Silva <fabiano.msrc@outlook.com>
2022-11-24 16:40:07 -0300

    Added basic database schema

---
+++ sql/my_routine_schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS `my_routine` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `my_routine`;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL DEFAULT '',
+  `password` varchar(150) NOT NULL DEFAULT '',reorganização do projeto
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `UQ_username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `task_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL DEFAULT 0,
+  `task_name` varchar(50) NOT NULL DEFAULT '',
+  `task_description` tinytext NULL DEFAULT NULL,
+  `task_status` varchar(20) NOT NULL DEFAULT '',
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  PRIMARY KEY (`task_id`),
+  KEY `FK_tasks_user_id` (`user_id`),
+  CONSTRAINT `FK_tasks_user_id` FOREIGN KEY `user_id` (`user_id`)
+  REFERENCES `users` (`user_id`)
+    ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
