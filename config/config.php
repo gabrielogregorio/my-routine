1d5d3e9d5bec53547cb4bafb97b6931c262fdd70
Fabiano Silva <fabiano.msrc@outlook.com>
2022-11-26 20:39:51 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 
-define('APPNAME', 'My Routine');
+define('APPNAME', 'My Daily Routine');
 define('BASEURL', 'http://127.0.0.1:8000');
 
 define('SESSION_STORAGE', dirname(__DIR__) . '/runtime/sessions/');
 
-define('HOSTNAME', '127.0.0.1');
-define('USERNAME', 'app');
-define('PASSWORD', 'y250jLIziNzG');
-define('DATABASE', 'my_routine');
-define('PORT', '3310');
+define('HOSTNAME', 'MYSERVERNAME');
+define('USERNAME', 'databaseUsername');
+define('PASSWORD', 'yourPasswordHere');
+define('DATABASE', 'yourDatabaseName');
+define('PORT', 3310);
 define('CHARSET', 'utf8mb4');
