f8ddf457488f1c42f82299a18ce332a2cf983801
Fabiano Silva <fabiano.msrc@outlook.com>
2022-11-26 09:23:53 -0300

    Project configuration file

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('APPNAME', 'My Routine');
+define('BASEURL', 'http://127.0.0.1:8000');
+
+define('SESSION_STORAGE', dirname(__DIR__) . '/runtime/sessions/');
+
+define('HOSTNAME', '0.0.0.0');
+define('USERNAME', 'app');
+define('PASSWORD', 'yourPasswordHere');
+define('DATABASE', 'yourDatabaseName');
+define('PORT', 3308);
+define('CHARSET', 'utf8mb4');
