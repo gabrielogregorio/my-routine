0b801deb81feebfedf25500e91b600d19d830df2
Fabiano Silva <fabiano.msrc@outlook.com>
2022-11-26 21:53:23 -0300

    Added helper functions to manipulate user registration password

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -15,3 +15,55 @@ if (! function_exists('env')) {
         return $_ENV[$key] ?? $default;
     }
 }
+
+if (! function_exists('password_is_valid')) {
+    /**
+     * @param string $password
+     * @return boolean
+     */
+    function password_is_valid(string $password): bool
+    {
+        if (password_get_info($password['algo'])) {
+            return true;
+        }
+
+        if (mb_strlen($password) >= PASSWORD_MIN_LEN && mb_strlen($password) <= PASSWORD_MAX_LEN) {
+            return true;
+        }
+        return false;
+    }
+}
+
+if (! function_exists('password_encryption')) {
+    /**
+     * @param string $password
+     * @return string
+     */
+    function password_encryption(string $password): string
+    {
+        return password_hash($password, PASSWORD_ENCRYPT, PASSWORD_OPTIONS);
+    }
+}
+
+if (! function_exists('password_checker')) {
+    /**
+     * @param string $password
+     * @param string $hash
+     * @return boolean
+     */
+    function password_checker(string $password, string $hash): bool
+    {
+        return password_verify($password, $hash);
+    }
+}
+
+if (! function_exists('password_rehash')) {
+    /**
+     * @param string $hash
+     * @return boolean
+     */
+    function password_rehash(string $hash): bool
+    {
+        return password_needs_rehash($hash, PASSWORD_ENCRYPT, PASSWORD_OPTIONS);
+    }
+}
